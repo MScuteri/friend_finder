8fdd738dad6b0ec1c40c216d4e062600b0d4c871
MattScuteri <mttscuteri@gmail.com>
2018-03-02 15:14:38 -0500

    routing files being built

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,11 @@
 //import server.js file
 const server = require('././server.js');
+const friends = require('./data/friends.js')
+
+app.get("/api/friends", function(req, res) {
+	res.json(availableFriends);
+})
+
+app.post("/api/friends", function(req, res) {
+	
+})
